abfafd1c2a1a4eb6dae505b2a88121576072a3c1
Philip Rosenberg <philip@rosenberg.dev>
2020-10-12 20:25:36 +0200

    Fix symlinks and use default config files

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,24 @@
-FROM frolvlad/alpine-glibc
+FROM frolvlad/alpine-glibc:alpine-3.12
 
 ARG VERSION
-ENV VERSION ${VERSION:-0.17.69}
-ENV SAVE ${SAVE} 
+ENV VERSION=${VERSION:-1.0.0}
+ENV SAVE=${SAVE} 
 ENV PORT=34197
 
-
-RUN rm -rf /var/cache/apk/* && rm -rf /tmp/* && apk update
-RUN apk add --virtual build-dependencies curl
-RUN apk add bash
-
-RUN mkdir -p /opt /factorio
-RUN curl -sSLf "https://www.factorio.com/get-download/$VERSION/headless/linux64" -o "/tmp/factorio-$VERSION.tar.xz"
-RUN tar -xf "/tmp/factorio-$VERSION.tar.xz" --directory /opt
-
-RUN apk del build-dependencies
+RUN set -euo pipefail \
+    && rm -rf /var/cache/apk/* \
+    && rm -rf /tmp/* \
+    && apk add --update --no-cache --no-progress --virtual build-dependencies curl \
+    && apk add --no-cache --no-progress bash \
+    && mkdir -p /opt/factorio /factorio \
+    && curl -sSLf "https://www.factorio.com/get-download/$VERSION/headless/linux64" -o "/tmp/factorio-$VERSION.tar.xz" \
+    && tar -xf "/tmp/factorio-$VERSION.tar.xz" --directory /opt \
+    && rm -f "/tmp/factorio-$VERSION.tar.xz" \
+    && apk del build-dependencies \
+    && ln -s /factorio/config /opt/factorio/config \
+    && ln -s /factorio/mods /opt/factorio/mods \
+    && ln -s /factorio/saves /opt/factorio/saves \
+    && ln -s /factorio/scenarios /opt/factorio/scenarios
 
 COPY ["entrypoint.sh", "/"]
 
@@ -23,6 +27,5 @@ RUN chmod +x /entrypoint.sh
 EXPOSE 34197/udp
 
 VOLUME /factorio
-
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
-
-ln -s /factorio/config /opt/factorio
-ln -s /factorio/data /opt/factorio
-ln -s /factorio/saves /opt/factorio
-ln -s /factorio/mods /opt/factorio
 set -eu
 
-# Create Map if it does not exist
-if [[ ! -f /opt/factorio/saves/${SAVE} ]]; then
-	echo creating map
-	/opt/factorio/bin/x64/factorio \
-		--create "/opt/factorio/saves/${SAVE}" \
-        	--map-gen-settings /opt/factorio/config/map-gen-settings.json
+mkdir -p /factorio/config /factorio/mods /factorio/saves /factorio/scenarios
+
+if [[ ! -f /opt/factorio/config/map-gen-settings.json ]]; then
+    echo "/opt/factorio/data/map-gen-settings.json not found, copying example file"
+    cp /opt/factorio/data/map-gen-settings.example.json /opt/factorio/config/map-gen-settings.json
 fi
+
+if [[ ! -f /opt/factorio/config/map-settings.json ]]; then
+    echo "/opt/factorio/data/map-settings.json not found, copying example file"
+    cp /opt/factorio/data/map-settings.example.json /opt/factorio/config/map-settings.json
+fi
+
+if [[ ! -f /opt/factorio/config/server-settings.json ]]; then
+    echo "/opt/factorio/data/server-settings.json not found, copying example file"
+    cp /opt/factorio/data/server-settings.example.json /opt/factorio/config/server-settings.json
+fi
+
+if [[ ! -f /opt/factorio/config/server-whitelist.json ]]; then
+    echo "/opt/factorio/data/server-whitelist.json not found, copying example file"
+    cp /opt/factorio/data/server-whitelist.example.json /opt/factorio/config/server-whitelist.json
+fi
+
+if [[ ! -f /opt/factorio/config/server-adminlist.json ]]; then
+    echo "/opt/factorio/data/server-adminlist.json not found, copying example file"
+    cp /opt/factorio/data/server-whitelist.example.json /opt/factorio/config/server-adminlist.json
+fi
+
+sed -i '/write-data=/c\write-data=\/factorio/' /opt/factorio/config/config.ini
+
+if [[ ! -f /opt/factorio/saves/${SAVE} ]]; then
+    echo "creating map ${SAVE}"
+    /opt/factorio/bin/x64/factorio \
+        --create "/factorio/saves/${SAVE}" \
+        --map-gen-settings /factorio/config/map-gen-settings.json
+fi
+
 /opt/factorio/bin/x64/factorio \
-	--start-server "/opt/factorio/saves/${SAVE}" \ 
-	--server-adminlist /opt/factorio/config/server-adminlist.json \
-	--server-settings /opt/factorio/config/server-settings.json
+    --start-server "/factorio/saves/${SAVE}" \
+    --server-adminlist /factorio/config/server-adminlist.json \
+    --server-settings /factorio/config/server-settings.json
